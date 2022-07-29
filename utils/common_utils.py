269f233660f7176c8ad4feced903d22e0bcbc671
Wang-weixi <69105805+Wang-weixi@users.noreply.github.com>
2022-07-29 08:31:12 +0800

    Add files via upload

---
+++ utils/common_utils.py
@@ -0,0 +1,328 @@
+import torch
+import torchvision
+import numpy as np
+from PIL import Image
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def crop_image(img, d=32):
+    '''Make dimensions divisible by `d`'''
+
+    new_size = (img.size[0] - img.size[0] % d, 
+                img.size[1] - img.size[1] % d)
+
+    bbox = [
+            int((img.size[0] - new_size[0])/2), 
+            int((img.size[1] - new_size[1])/2),
+            int((img.size[0] + new_size[0])/2),
+            int((img.size[1] + new_size[1])/2),
+    ]
+
+    img_cropped = img.crop(bbox)
+    return img_cropped
+
+def get_params(opt_over, net, net_kernel, downsampler=None):
+    '''Returns parameters that we want to optimize over.
+
+    Args:
+        opt_over: comma separated list, e.g. "net,input" or "net"
+        net: network
+        net_input: torch.Tensor that stores input `z`
+    '''
+    opt_over_list = opt_over.split(',')
+    params = []
+    
+    for opt in opt_over_list:
+    
+        if opt == 'net':
+            params += [x for x in net.parameters() ]
+        elif  opt=='down':
+            assert downsampler is not None
+            params = [x for x in downsampler.parameters()]
+        elif opt == 'net_kernel':
+            params += [x for x in net_kernel.parameters() ]
+        else:
+            assert False, 'what is it?'
+            
+    return params
+
+def get_image_grid(images_np, nrow=8):
+    '''Creates a grid from a list of images by concatenating them.'''
+    images_torch = [torch.from_numpy(x) for x in images_np]
+    torch_grid = torchvision.utils.make_grid(images_torch, nrow)
+    
+    return torch_grid.numpy()
+
+def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
+    """Draws images in a grid
+    
+    Args:
+        images_np: list of images, each image is np.array of size 3xHxW of 1xHxW
+        nrow: how many images will be in one row
+        factor: size if the plt.figure 
+        interpolation: interpolation used in plt.imshow
+    """
+    n_channels = max(x.shape[0] for x in images_np)
+    assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
+    
+    images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
+
+    grid = get_image_grid(images_np, nrow)
+    
+    plt.figure(figsize=(len(images_np) + factor, 12 + factor))
+    
+    if images_np[0].shape[0] == 1:
+        plt.imshow(grid[0], cmap='gray', interpolation=interpolation)
+    else:
+        plt.imshow(grid.transpose(1, 2, 0), interpolation=interpolation)
+    
+    plt.show()
+    
+    return grid
+
+def load(path):
+    """Load PIL image."""
+    img = Image.open(path)
+    return img
+
+def get_image(path, imsize=-1):
+    """Load an image and resize to a cpecific size. 
+
+    Args: 
+        path: path to image
+        imsize: tuple or scalar with dimensions; -1 for `no resize`
+    """
+    img = load(path)
+
+    if isinstance(imsize, int):
+        imsize = (imsize, imsize)
+
+    if imsize[0]!= -1 and img.size != imsize:
+        if imsize[0] > img.size[0]:
+            img = img.resize(imsize, Image.BICUBIC)
+        else:
+            img = img.resize(imsize, Image.ANTIALIAS)
+
+    img_np = pil_to_np(img)
+
+    return img, img_np
+
+
+
+def fill_noise(x, noise_type):
+    """Fills tensor `x` with noise of type `noise_type`."""
+    if noise_type == 'u':
+        x.uniform_()
+    elif noise_type == 'n':
+        x.normal_() 
+    else:
+        assert False
+
+def get_noise(input_depth, method, spatial_size, noise_type='u', var=1./10):
+    """Returns a pytorch.Tensor of size (1 x `input_depth` x `spatial_size[0]` x `spatial_size[1]`) 
+    initialized in a specific way.
+    Args:
+        input_depth: number of channels in the tensor
+        method: `noise` for fillting tensor with noise; `meshgrid` for np.meshgrid
+        spatial_size: spatial size of the tensor to initialize
+        noise_type: 'u' for uniform; 'n' for normal
+        var: a factor, a noise will be multiplicated by. Basically it is standard deviation scaler. 
+    """
+    if isinstance(spatial_size, int):
+        spatial_size = (spatial_size, spatial_size)
+    if method == 'noise':
+        shape = [1, input_depth, spatial_size[0], spatial_size[1]]
+        net_input = torch.zeros(shape)
+        
+        fill_noise(net_input, noise_type)
+        net_input *= var            
+    elif method == 'meshgrid': 
+        assert input_depth == 2
+        X, Y = np.meshgrid(np.arange(0, spatial_size[1])/float(spatial_size[1]-1), np.arange(0, spatial_size[0])/float(spatial_size[0]-1))
+        meshgrid = np.concatenate([X[None,:], Y[None,:]])
+        net_input=  np_to_torch(meshgrid)
+    else:
+        assert False
+        
+    return net_input
+
+def pil_to_np(img_PIL):
+    '''Converts image in PIL format to np.array.
+    
+    From W x H x C [0...255] to C x W x H [0..1]
+    '''
+    ar = np.array(img_PIL)
+
+    if len(ar.shape) == 3:
+        ar = ar.transpose(2,0,1)
+    else:
+        ar = ar[None, ...]
+
+    return ar.astype(np.float32) / 255.
+
+def np_to_pil(img_np): 
+    '''Converts image in np.array format to PIL image.
+    
+    From C x W x H [0..1] to  W x H x C [0...255]
+    '''
+    ar = np.clip(img_np*255,0,255).astype(np.uint8)
+    
+    if img_np.shape[0] == 1:
+        ar = ar[0]
+    else:
+        ar = ar.transpose(1, 2, 0)
+
+    return Image.fromarray(ar)
+
+def np_to_torch(img_np):
+    '''Converts image in numpy.array to torch.Tensor.
+
+    From C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return torch.from_numpy(img_np)[None, :]
+
+def torch_to_np(img_var):
+    '''Converts an image in torch.Tensor format to np.array.
+
+    From 1 x C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return img_var.detach().cpu().numpy()[0]
+
+
+class Adam(torch.optim.Optimizer):
+    r"""Implements Adam algorithm.
+
+    It has been proposed in `Adam: A Method for Stochastic Optimization`_.
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        amsgrad (boolean, optional): whether to use the AMSGrad variant of this
+            algorithm from the paper `On the Convergence of Adam and Beyond`_
+            (default: False)
+
+    .. _Adam\: A Method for Stochastic Optimization:
+        https://arxiv.org/abs/1412.6980
+    .. _On the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
+    """
+
+    def __init__(self, noise_level,params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+                 weight_decay=0, amsgrad=False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay, amsgrad=amsgrad)
+        super(Adam, self).__init__(params, defaults)
+        self.noise_level=noise_level
+    def __setstate__(self, state):
+        super(Adam, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('amsgrad', False)
+
+    @torch.no_grad()
+    
+    def step(self, loss_data,noise_type='gaussian',closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad
+                ssize=grad.size()
+                #zzero=torch.zeros(ssize).cuda()
+                if grad.is_sparse:
+                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                amsgrad = group['amsgrad']
+                #norm=torch.norm(grad)
+                #print('grad_norm',norm)
+                noise = torch.cuda.FloatTensor(ssize)
+                #torch.randn(ssize, out=noise)
+                if noise_type=='poission':
+                    torch.randn(ssize, out=noise)  
+                    possion=torch.poisson(grad)
+                    if torch.norm(possion)>1*torch.norm(grad):
+                        possion=possion-grad
+                        noise=10*possion
+                if noise_type=='gaussian':
+                    torch.randn(ssize, out=noise)
+                if noise_type=='bernoulli':
+                    torch.ones(ssize,out=noise)
+                    noise=0.1*noise
+                    torch.bernoulli(noise, out=noise)
+                    noise=-noise*grad
+                if noise_type=='levi':
+                    torch.randn(ssize, out=noise)  
+                    possion=torch.poisson(grad)
+                    if torch.norm(possion)>1*torch.norm(grad):
+                        possion=possion-grad
+                        noise=30*possion+noise
+                            
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    if amsgrad:
+                        # Maintains max of all exp. moving avg. of sq. grad. values
+                        state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                if amsgrad:
+                    max_exp_avg_sq = state['max_exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(p, alpha=group['weight_decay'])
+
+                # Decay the first and second moment running average coefficient
+
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg=exp_avg+(1 - beta1)*loss_data*self.noise_level*noise
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+                if amsgrad:
+                    # Maintains the maximum of all 2nd moment running avg. till now
+                    torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
+                    # Use the max. for normalizing running avg. of gradient
+                    denom = (max_exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+                else:
+                    denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+
+                step_size = group['lr'] / bias_correction1
+
+                p.addcdiv_(exp_avg, denom, value=-step_size)
+        return loss
